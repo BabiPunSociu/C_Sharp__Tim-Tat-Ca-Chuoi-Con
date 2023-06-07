3df4b9f2a51235a09c2f4708bfe45962920e61c8
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:59:57 +0700

    DungNguyen

---
+++ TimTatCaChuoiCon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimTatCaChuoiCon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập chuỗi: ");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine("Tất cả các chuỗi con là: ");
+
+            for(int i=0;i < inputString.Length;i++)
+            {
+                StringBuilder subString = new StringBuilder(inputString.Length - i);
+                for(int j=i;j < inputString.Length;j++)
+                {
+                    subString.Append(inputString[j]);
+                    Console.Write(subString + " ");
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+}
